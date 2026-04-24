1fecf42cba43e2c9c1d4312b579359eb1e1538da
Shoaib Akhtar <shoaibakht34@gmail.com>
2026-04-25 02:32:02 +0530

    Remove merge conflict markers from api.js

--- url-shortener-frontend/src/api/api.js
+++ url-shortener-frontend/src/api/api.js
@@ -2,8 +2,4 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL || "https://url-shortener-psx0.onrender.com",
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 2170cee036fff3465f38fae0c9a11543039f3f34
